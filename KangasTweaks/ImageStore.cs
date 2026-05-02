25ff317733633a1a894f6367b6ef0962afe9bcb1
Zach Kangas <zkangas6@gmail.com>
2026-05-02 12:19:36 -0500

    api 15

--- KangasTweaks/ImageStore.cs
+++ KangasTweaks/ImageStore.cs
@@ -1,11 +1,6 @@
-﻿using System.Collections.Generic;
-using Dalamud.Data;
-using Dalamud.Interface.Internal;
-using Dalamud.Interface.Textures.TextureWraps;
+﻿using Dalamud.Interface.Textures.TextureWraps;
 using Dalamud.Plugin;
 using Dalamud.Plugin.Services;
-using Dalamud.Utility;
-using ImGuiScene;
 
 namespace KangasTweaks;
 
